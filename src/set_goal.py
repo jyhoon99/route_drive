769fbf4fce0885c0825afdf41af513eb928e3fc5
jyhoon99 <jyhoon99@naver.com>
2023-09-01 08:07:27 +0900

    first commit

---
+++ src/set_goal.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import PoseStamped
+
+# Callbacks definition
+
+def done_cb(status, result):
+    if status == 3:
+        rospy.loginfo("Goal reached")
+    elif status == 2 or status == 8:
+        rospy.loginfo("Goal cancelled")
+    elif status == 4:
+        rospy.loginfo("Goal aborted")
+
+def active_cb():
+    rospy.loginfo("Goal pose being processed")
+
+def feedback_cb(feedback):
+    pass  # You can process feedback here if needed
+
+rospy.init_node('send_goal')
+
+navclient = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+navclient.wait_for_server()
+
+# Example of navigation goal
+goal = MoveBaseGoal()
+goal.target_pose.header.frame_id = "map"
+goal.target_pose.header.stamp = rospy.Time.now()
+
+goal.target_pose.pose.position.x = 0.500
+goal.target_pose.pose.position.y = 0.500
+goal.target_pose.pose.position.z = 0.0
+goal.target_pose.pose.orientation.x = 0.0
+goal.target_pose.pose.orientation.y = 0.0
+goal.target_pose.pose.orientation.z = 0.662
+goal.target_pose.pose.orientation.w = 0.750
+
+navclient.send_goal(goal, done_cb=done_cb, active_cb=active_cb, feedback_cb=feedback_cb)
+finished = navclient.wait_for_result()
+
+if not finished:
+    rospy.logerr("Action server not available!")
+else:
+    rospy.loginfo(navclient.get_result())
